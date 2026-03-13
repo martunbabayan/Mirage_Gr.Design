979cf2c85ed46293f74a68cff0a6529ac1554a5f
martunbabayan <martunbabayan02@gmail.com>
2026-03-13 16:57:54 +0400

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const app = require("./src/app");
-
-const PORT = Number(process.env.PORT || 3001);
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
